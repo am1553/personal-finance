eaf2b2a8a7adb8d5b54ffda49af066518503c0ac
Aryan Mehta <aryanmehta2016@gmail.com>
2024-09-05 14:53:42 +0100

    fixing auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,13 +1,8 @@
-import AuthForm from "@/components/forms/AuthForm";
 import { SignIn } from "@clerk/nextjs";
 import React from "react";
 
 function LoginPage() {
-  return (
-    <AuthForm type="login">
-      <SignIn routing="hash" />
-    </AuthForm>
-  );
+  return <SignIn routing="hash" />;
 }
 
 export default LoginPage;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,8 @@
-import AuthForm from "@/components/forms/AuthForm";
+import { SignUp } from "@clerk/nextjs";
 import React from "react";
 
 function SignupPage() {
-  return <AuthForm type="signup" />;
+  return <SignUp routing="hash" />;
 }
 
 export default SignupPage;
